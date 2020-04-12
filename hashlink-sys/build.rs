b3f421f11883e5233b5bc5247a7efeb3aa4b8b24
Andreas Fischer <home@deviru.de>
2020-04-12 23:01:24 +0200

    push progress

--- hashlink-sys/build.rs
+++ hashlink-sys/build.rs
@@ -4,44 +4,47 @@ use std::path::PathBuf;
 use bindgen;
 use cmake::Config;
 
-fn main() {
+fn build_libhl_full()
+{
+    let mut build = cc::Build::new();
 
-    // TODO make these flags cargo features and document how to add
-    // dependencies (e.g. see hashlink docs)
-    let output_path = Config::new("../vendor/hashlink")
-        .define("WITH_DIRECTX", "false")
-        .define("WITH_FMT", "false")
-        .define("WITH_OPENAL", "false")
-        .define("WITH_SDL", "false")
-        .define("WITH_SQLITE", "false")
-        .define("WITH_SSL", "false")
-        .define("WITH_UI", "false")
-        .define("WITH_UV", "false")
-        .define("WITH_VIDEO", "false")
-        .define("BUILD_TESTING", "false")
-        .build();
+    // Global config
+    build.cpp        (true);
+    build.static_flag(true);
+    build.out_dir    ("_deploy");
 
-    let mut output_lib_path = output_path.clone();
-    output_lib_path.push("lib");
+    // C-Flags    
+    build.flag("-std=c++11");
 
-    let mut output_bin_path = output_path.clone();
-    output_bin_path.push("bin");
+    // -I
+    build.include("../vendor/hashlink/src");
+    build.include("../vendor/hashlink/include/pcre");
 
-    println!("cargo:rustc-link-search=native={}", output_lib_path.display());
-    println!("cargo:rustc-link-search=native={}", output_bin_path.display());
+    let std_src = [
+          "wrapper.c"
+    ];
+
+    for source_file in &std_src {
+        build.file(&source_file);
+    }
+
+    // Linker configuration 
+    build.static_crt(true);
+
+    // -L
+    // -l
     
-    println!("cargo:rustc-link-lib=static=libhl");
-    println!("cargo:rustc-link-lib=dylib=libhl");
-    println!("cargo:rustc-link-lib=dylib=ws2_32");
-    println!("cargo:rustc-link-lib=dylib=user32");
+    // GO
+    build.compile("hlfull");
+}
 
-    let mut output_include_path = output_path.clone();
-    output_include_path.push("include");
-    println!("cargo:include={}", output_include_path.display());
+fn main() {
+    build_libhl_full();
+    println!("cargo:rustc-link-search=./_deploy");
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg(format!("--include-directory={}", output_include_path.display()))
+        .clang_arg("--include-directory=../vendor/hashlink/src")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
